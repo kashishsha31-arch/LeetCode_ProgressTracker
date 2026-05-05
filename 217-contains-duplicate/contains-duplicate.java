05ffd49ddcec35d67850a4d02988aa31cc411c4f
kashishsha31-arch <kashishsha31@gmail.com>
2026-05-05 12:16:56 -0700

    Time: 22 ms (29.87%) | Memory: 81 MB (74.48%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+             hs.add(nums[i]);
+        }
+        if(hs.size()==nums.length)
+        {
+            return false;
+        }
+        return true;
+        
+    }
+}
