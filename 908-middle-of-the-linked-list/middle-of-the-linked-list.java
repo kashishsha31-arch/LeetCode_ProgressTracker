08c9c021f3fd1a63d471976cb5dda578eb13014b
kashishsha31-arch <kashishsha31@gmail.com>
2026-05-08 10:37:46 -0700

    Time: 0 ms (100.00%) | Memory: 43 MB (16.15%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -12,21 +12,34 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         //first solution
         //using length of linked list
-        int n=0;
-        ListNode tmp=head;
-        while(tmp!=null)
-        {
-            n++;
-            tmp=tmp.next;
-        }
-        int mid=n/2;
-        tmp=head;
-        while(mid>0)
-        {
-                tmp=tmp.next;
-                mid --;
-        }
-        return tmp;
-        
+        // int n=0;
+        // ListNode tmp=head;
+        // while(tmp!=null)
+        // {
+        //     n++;
+        //     tmp=tmp.next;
+        // }
+        // int mid=n/2;
+        // tmp=head;
+        // while(mid>0)
+        // {
+        //         tmp=tmp.next;
+        //         mid --;
+        // }
+        // return tmp;
+
+
+        //OPTIMAL APPROACH FOR FINDING TRHE MIID OF LL
+        //SLOW FAST POINTER APPROACH
+        ListNode slow,fast;
+        slow=head;
+        fast=head;
+        //slow will take one step and fast will take steps 
+         while(fast!=null && fast.next!=null)
+         {
+            slow=slow.next;
+            fast=fast.next.next;
+         }
+        return slow;
             }
 }
