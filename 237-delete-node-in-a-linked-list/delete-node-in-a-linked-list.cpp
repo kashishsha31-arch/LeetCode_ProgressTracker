a5a80331012e2b58aa4ef26e897856fd56ab79aa
kashishsha31-arch <kashishsha31@gmail.com>
2026-05-08 09:04:21 -0700

    Time: 10 ms (37.63%) | Memory: 12.4 MB (54.59%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -8,10 +8,19 @@
  */
 class Solution {
 public:
+    // void deleteNode(ListNode* node) {
+    //     swap(node->val,node->next->val);
+    //     ListNode* temp=node->next;
+    //     node->next=node->next->next;//here we are letting our currr node point to the other node which is 1 ahead of the node to be deleted 
+    //     delete temp;
+    // }
     void deleteNode(ListNode* node) {
-        swap(node->val,node->next->val);
-        ListNode* temp=node->next;
-        node->next=node->next->next;//here we are letting our currr node point to the other node which is 1 ahead of the node to be deleted 
-        delete temp;
+        node->val=node->next->val;//copyingh the same val in curr
+       ListNode* temp=node->next;
+       node->next=node->next->next;
+       delete temp;
+
     }
+
+
 };
